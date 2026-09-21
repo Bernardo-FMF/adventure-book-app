d501e04138e75028c88f0c307bb4d801a4973257
Bernardo-FMF <bernardo_fragoso91@hotmail.com>
2026-09-21 13:59:55 +0100

    Made username case sensitive.

--- backend/src/main/resources/db/migration/V3__addPlayer.sql
+++ backend/src/main/resources/db/migration/V3__addPlayer.sql
@@ -6,9 +6,10 @@ CREATE TABLE player
     CONSTRAINT pk_player PRIMARY KEY (id)
 );
 
-CREATE UNIQUE INDEX uq_player_username ON player (lower(username));
+CREATE UNIQUE INDEX uq_player_username ON player (username);
 
 ALTER TABLE game_session
-    ADD COLUMN player_id BIGINT NOT NULL, ADD CONSTRAINT fk_game_player FOREIGN KEY (player_id) REFERENCES player (id) ON DELETE CASCADE;
+    ADD COLUMN player_id BIGINT,
+    ADD CONSTRAINT fk_game_player FOREIGN KEY (player_id) REFERENCES player (id) ON DELETE CASCADE;
 
 CREATE UNIQUE INDEX uq_active_game_per_book ON game_session (player_id, book_id) WHERE status = 'IN_PROGRESS';
