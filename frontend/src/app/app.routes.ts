d87e3e765d5a556be7eceea41ab0c3f0a3e5e4c3
Bernardo-FMF <bernardo_fragoso91@hotmail.com>
2026-09-21 12:47:25 +0100

    Added user header layout.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,18 +4,24 @@ import { playerGuard } from './core/state/player-guard';
 
 export const routes: Routes = [
   {
-    path: '',
-    loadComponent: () => import('./books/books-page').then((m) => m.BooksPage),
-  },
-  {
     path: 'adventurer',
     canActivate: [anonymousGuard],
     loadComponent: () => import('./adventurer/adventurer-page').then((m) => m.AdventurerPage),
   },
   {
-    path: 'games/:gameId',
-    canActivate: [playerGuard],
-    loadComponent: () => import('./game/game-page').then((m) => m.GamePage),
+    path: '',
+    loadComponent: () => import('./layout/layout').then((m) => m.Layout),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./books/books-page').then((m) => m.BooksPage),
+      },
+      {
+        path: 'games/:gameId',
+        canActivate: [playerGuard],
+        loadComponent: () => import('./game/game-page').then((m) => m.GamePage),
+      },
+      { path: '**', redirectTo: '' },
+    ],
   },
-  { path: '**', redirectTo: '' },
 ];
